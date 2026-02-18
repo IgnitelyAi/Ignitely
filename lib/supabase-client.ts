0c8d6b163794b71d64b27c2464e5fb1a63038d2d
Walid Sefraoui <walidsefraoui@MacBook-Air-van-Walid.local>
2026-02-18 23:57:28 +0100

    force client only supabase

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,14 +1,12 @@
 import { createClient } from "@supabase/supabase-js"
 
-let supabase: ReturnType<typeof createClient> | null = null
-
 export function getSupabase() {
-  if (!supabase) {
-    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
-
-    supabase = createClient(supabaseUrl, supabaseAnonKey)
+  if (typeof window === "undefined") {
+    throw new Error("Supabase mag alleen in de browser gebruikt worden.")
   }
 
-  return supabase
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+  return createClient(supabaseUrl, supabaseAnonKey)
 }
