6a14560fecccf20ab71673ecfaecb9a39ecb17db
Walid Sefraoui <walidsefraoui@MacBook-Air-van-Walid.local>
2026-02-18 01:55:20 +0100

    hard reset app

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,3 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-
 export default function Home() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    );
-
-    const fetchData = async () => {
-      const { data } = await supabase.from("test").select("*");
-      setData(data);
-    };
-
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  );
+  return <div>WORKING</div>;
 }
