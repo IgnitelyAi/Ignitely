2f45fb1bdc3094136fff04dfb62b3dbb5aed0fd9
Walid Sefraoui <walidsefraoui@MacBook-Air-van-Walid.local>
2026-02-18 02:24:08 +0100

    final fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,3 @@
-export const dynamic = "force-dynamic";
-
-import { getSupabase } from "@/lib/supabase";
-
-export default async function Home() {
-  const supabase = getSupabase();
-
-  if (!supabase) {
-    return <div>Supabase not configured</div>;
-  }
-
-  const { data, error } = await supabase
-    .from("test")
-    .select("*");
-
-  if (error) {
-    return <div>Database error</div>;
-  }
-
-  return (
-    <div>
-      <h1>WORKING</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  );
+export default function Home() {
+  return <div>WORKING</div>;
 }
